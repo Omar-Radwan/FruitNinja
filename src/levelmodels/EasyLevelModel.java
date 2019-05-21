4a0a1299c5c7857932f65a7d6cf3839d8efca91d
Omar Radwan <Omarradwanx@gmail.com>
2019-05-22 01:03:25 +0200

    esraa updates

--- src/levelmodels/EasyLevelModel.java
+++ src/levelmodels/EasyLevelModel.java
@@ -3,12 +3,12 @@ public class EasyLevelModel extends LevelModel {
 
 	public EasyLevelModel() {
 		super();
-		repeatDur = 1000;
+		repeatDur = 1500;
 		fataldur = 7000;
 		normaldur = 6000;
 		pathFruitDur = 3000;
-		pathFatalDur = 3000;
-		pathNormalDur = 3000;
+		pathFatalDur = 3200;
+		pathNormalDur = 3500;
 		fileName = "easyscore.txt";
 	}
 }
